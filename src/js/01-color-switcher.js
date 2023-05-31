411c5b77e4a38a5819381fc31906087b326aed1d
Andriy Lobash <110089586+Ragnareock@users.noreply.github.com>
2023-06-01 02:11:49 +0300

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
 
+startBtnEl.addEventListener('click', onSwitchStart);
+stopBtnEl.addEventListener('click', onSwitchStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let timerId = null;
+stopBtnEl.setAttribute('disabled', true);
+
+function switchColorBcg() {
+  return (document.body.style.backgroundColor = getRandomHexColor());
+}
+
+function onSwitchStart() {
+  timerId = setInterval(switchColorBcg, 1000);
+  stopBtnEl.removeAttribute('disabled');
+  startBtnEl.setAttribute('disabled', true);
+}
+
+function onSwitchStop() {
+  clearInterval(timerId);
+  startBtnEl.removeAttribute('disabled');
+  stopBtnEl.setAttribute('disabled', true);
+}
